f3179380189cd879c4c9dbb201a19fad69e663dd
guilhermetborges <guididibem@gmail.com>
2026-05-06 17:11:02 -0300

    add Meta Pixel tracking to landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,8 @@
+import Script from 'next/script';
+
 import './globals.css';
+
+const META_PIXEL_ID = '26795304880079670';
 
 export const metadata = {
   title: 'Deboxe x Citrine | deboxesports Fitness Premium',
@@ -13,7 +17,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <head>
+        <Script id="meta-pixel" strategy="beforeInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');`}
+        </Script>
+      </head>
+      <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
